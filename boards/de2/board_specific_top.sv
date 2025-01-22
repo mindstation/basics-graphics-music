39d3bed6fca42421905502982599aa112a9fd6e9
Alexander Kirichenko <aakirichenko@google.com>
2025-01-22 10:04:15 +0500

    de2: I2S GPIO output recovered

--- boards/de2/board_specific_top.sv
+++ boards/de2/board_specific_top.sv
@@ -13,6 +13,7 @@ module board_specific_top
               w_gpio        = 72,
 
               // gpio 0..5 are reserved for INMP 441 I2S microphone.
+              // Odd gpio 27..33 are reserved I2S audio.
 
               screen_width  = 640,
               screen_height = 480,
@@ -295,21 +296,39 @@ module board_specific_top
 
     `ifdef INSTANTIATE_SOUND_OUTPUT_INTERFACE_MODULE
 
-        // DE2 onboard audio codec: WM8731
+        wire mclk;
+        wire bclk;
+        wire lrclk;
+        wire i2s_dac_data;
+
         i2s_audio_out
         # (
-            .clk_mhz ( clk_mhz      )
+            .clk_mhz ( clk_mhz   )
         )
         i_audio_out
         (
             .clk     ( clk          ),
             .reset   ( rst          ),
             .data_in ( sound        ),
-            .mclk    ( AUD_XCK      ),
-            .bclk    ( AUD_BCLK     ),
-            .lrclk   ( AUD_DACLRCK  ),
-            .sdata   ( AUD_DACDAT   )
+            .mclk    ( mclk         ),
+            .bclk    ( bclk         ),
+            .lrclk   ( lrclk        ),
+            .sdata   ( i2s_dac_data )
         );
+
+        assign GPIO_1 [33] = mclk;
+        assign GPIO_1 [31] = bclk;
+        assign GPIO_1 [29] = i2s_dac_data;
+        assign GPIO_1 [27] = lrclk;
+
+        // VCC and GND for i2s_audio_out are on dedicated pins
+
+        // DE2 onboard audio codec: WM8731
+        // The audio codec interface
+        assign AUD_XCK     = mclk;
+        assign AUD_BCLK    = bclk;
+        assign AUD_DACLRCK = lrclk;
+        assign AUD_DACDAT  = i2s_dac_data;
 
         // The audio codec configuration
         I2C_AUDIO_Config
